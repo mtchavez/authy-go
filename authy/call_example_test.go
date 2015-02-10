ba94e0197cfcf674d39aac9c9b06617e380a8021
Chavez <matthew@el-chavez.me>
2015-02-10 08:12:48 -0800

    Call force

--- authy/call_example_test.go
+++ authy/call_example_test.go
@@ -14,3 +14,30 @@ func ExampleClient_Call() {
 	// success: true
 	// message: Call started...
 }
+
+func ExampleClient_Call_NotFound() {
+	client := NewSandboxClient("d57d919d11e6b221c9bf6f7c882028f9")
+	req := &CallReq{
+		AuthyId: 3,
+	}
+	resp := client.Call(req)
+	fmt.Println("success:", resp.Success)
+	fmt.Println("message:", resp.Message)
+	// Output:
+	// success: false
+	// message: User not found.
+}
+
+func ExampleClient_Force() {
+	client := NewSandboxClient("d57d919d11e6b221c9bf6f7c882028f9")
+	req := &CallReq{
+		AuthyId: 1,
+		Force:   true,
+	}
+	resp := client.Call(req)
+	fmt.Println("success:", resp.Success)
+	fmt.Println("message:", resp.Message)
+	// Output:
+	// success: true
+	// message: Call started...
+}
