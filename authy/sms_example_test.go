dcbb39da6baf7e3623d3e521cfe1ec3629cd48d9
Chavez <matthew@el-chavez.me>
2015-02-09 21:47:07 -0800

    SMS custom actions

--- authy/sms_example_test.go
+++ authy/sms_example_test.go
@@ -49,3 +49,19 @@ func ExampleClient_SMS_Shortcode() {
 	// message: SMS token was sent
 	// cellphone: +1-XXX-XXX-XX86
 }
+
+func ExampleClient_SMS_CustomAction() {
+	client := NewSandboxClient("d57d919d11e6b221c9bf6f7c882028f9")
+	req := &SMSReq{
+		AuthyId:      1,
+		CustomAction: "change_preferences",
+	}
+	resp := client.SMS(req)
+	fmt.Println("success:", resp.Success)
+	fmt.Println("message:", resp.Message)
+	fmt.Println("cellphone:", resp.Cellphone)
+	// Output:
+	// success: true
+	// message: SMS token was sent
+	// cellphone: +1-XXX-XXX-XX86
+}
