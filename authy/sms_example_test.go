ea13db8ccb8b507fd99aa5693be4e8ed3ab16a06
Chavez <matthew@el-chavez.me>
2015-02-09 21:40:24 -0800

    Force SMS

--- authy/sms_example_test.go
+++ authy/sms_example_test.go
@@ -16,3 +16,19 @@ func ExampleClient_SMS() {
 	// message: SMS token was sent
 	// cellphone: +1-XXX-XXX-XX86
 }
+
+func ExampleClient_SMS_Force() {
+	client := NewSandboxClient("d57d919d11e6b221c9bf6f7c882028f9")
+	req := &SMSReq{
+		AuthyId: 1,
+		Force:   true,
+	}
+	resp := client.SMS(req)
+	fmt.Println("success:", resp.Success)
+	fmt.Println("message:", resp.Message)
+	fmt.Println("cellphone:", resp.Cellphone)
+	// Output:
+	// success: true
+	// message: SMS token was sent
+	// cellphone: +1-XXX-XXX-XX86
+}
